fd131f5f5a00307c2653e73377d498d1347773f9
Sne-hi <snehikanaktopno@gmail.com>
2026-08-23 22:48:36 +0530

    Update project setup and requirements

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+__version__ = "0.0.1"
+
+REPO_NAME = "Text-Summarizer"
+AUTHOR_USER_NAME = "Sne-hi"
+SCR_REPO = "text-summarizer"
+AUTHOR_EMAIL = "snehiakanktopno@gmail.com"
+
+setuptools.setup(
+    name=SCR_REPO,  
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A simple text summarizer",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
